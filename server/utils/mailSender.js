9deca7485e68bc21bf9059c90db0e6abbf8a5969
Nikita Shinde <nikitashinde1705@gmail.com>
2026-02-27 22:36:37 +0530

    Nodmailer code added

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,70 +1,70 @@
-const sgMail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
+//this function is for send OTP via email
 const mailSender = async (email, title, body) => {
-  try {
-    const msg = {
-      to: email,
-      from: process.env.MAIL_USER, // verified sender email in SendGrid
-      subject: title,
-      html: body,
-    };
+    try{
+        // let transporter = nodemailer.createTransport({
+        //     host:process.env.MAIL_HOST,
+        //     auth:{
+        //         user: process.env.MAIL_USER,
+        //         pass: process.env.MAIL_PASS,
+        //     }
+        // })
 
-    const info = await sgMail.send(msg);
-    console.log("Email sent successfully");
-    return info;
+        let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS,
+        },
+        tls: {
+            rejectUnauthorized: false,
+        },
+        });
 
-  } catch (error) {
-    console.error("MAIL ERROR:", error);
-    throw error;
-  }
-};
+        let info = await transporter.sendMail({
+            from:'StudyNotion || CodHelp - by Nikita',
+            to:`${email}`,
+            subject:`${title}`,
+            html:`${body},`
+        })
+        console.log(info);
+        return info;
+    }
+
+    catch(error){
+        console.error("MAIL ERROR:", error);
+        throw error; // IMPORTANT
+        //console.log(error.message);
+    }
+}
 
 module.exports = mailSender;
 
 
-// const nodemailer = require("nodemailer");
+// const sgMail = require("@sendgrid/mail");
 
-// //this function is for send OTP via email
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 // const mailSender = async (email, title, body) => {
-//     try{
-//         // let transporter = nodemailer.createTransport({
-//         //     host:process.env.MAIL_HOST,
-//         //     auth:{
-//         //         user: process.env.MAIL_USER,
-//         //         pass: process.env.MAIL_PASS,
-//         //     }
-//         // })
+//   try {
+//     const msg = {
+//       to: email,
+//       from: process.env.MAIL_USER, // verified sender email in SendGrid
+//       subject: title,
+//       html: body,
+//     };
 
-//         let transporter = nodemailer.createTransport({
-//         host: "smtp.gmail.com",
-//         port: 587,
-//         secure: false,
-//         auth: {
-//             user: process.env.MAIL_USER,
-//             pass: process.env.MAIL_PASS,
-//         },
-//         tls: {
-//             rejectUnauthorized: false,
-//         },
-//         });
+//     const info = await sgMail.send(msg);
+//     console.log("Email sent successfully");
+//     return info;
 
-//         let info = await transporter.sendMail({
-//             from:'StudyNotion || CodHelp - by Nikita',
-//             to:`${email}`,
-//             subject:`${title}`,
-//             html:`${body},`
-//         })
-//         console.log(info);
-//         return info;
-//     }
-
-//     catch(error){
-//         console.error("MAIL ERROR:", error);
-//         throw error; // IMPORTANT
-//         //console.log(error.message);
-//     }
-// }
+//   } catch (error) {
+//     console.error("MAIL ERROR:", error);
+//     throw error;
+//   }
+// };
 
 // module.exports = mailSender;
