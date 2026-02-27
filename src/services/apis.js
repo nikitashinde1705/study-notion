73c1a8679ca7bf00e8d1d9dd5ca1dbfe6d21cc90
Nikita Shinde <nikitashinde1705@gmail.com>
2026-02-27 12:46:04 +0530

    Done with api fixing

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,13 +1,22 @@
 const BASE_URL = process.env.REACT_APP_BASE_URL
 
 // AUTH ENDPOINTS
+// export const endpoints = {
+//   SENDOTP_API: BASE_URL + "/auth/sendotp",
+//   SIGNUP_API: BASE_URL + "/auth/signup",
+//   LOGIN_API: BASE_URL + "/auth/login",
+//   RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
+//   RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
+// }
+
 export const endpoints = {
-  SENDOTP_API: BASE_URL + "/auth/sendotp",
-  SIGNUP_API: BASE_URL + "/auth/signup",
-  LOGIN_API: BASE_URL + "/auth/login",
-  RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
-  RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
+  SENDOTP_API: BASE_URL + "/api/v1/auth/sendotp",
+  SIGNUP_API: BASE_URL + "/api/v1/auth/signup",
+  LOGIN_API: BASE_URL + "/api/v1/auth/login",
+  RESETPASSTOKEN_API: BASE_URL + "/api/v1/auth/reset-password-token",
+  RESETPASSWORD_API: BASE_URL + "/api/v1/auth/reset-password",
 }
+
 
 // PROFILE ENDPOINTS
 export const profileEndpoints = {
